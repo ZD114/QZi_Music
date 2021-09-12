e29fb68b708ebc785ec91caf2c0748aa7c69e0f8
QZi <1140429329@qq.com>
2021-09-12 09:15:30 +0800

    第一次提交

--- src/main/java/com/zhangd/music/controller/SingerController.java
+++ src/main/java/com/zhangd/music/controller/SingerController.java
@@ -2,15 +2,16 @@ package com.zhangd.music.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.zhangd.music.bean.Singer;
+import com.zhangd.music.dao.SingerDao;
 import com.zhangd.music.service.SingerService;
 import com.zhangd.music.utils.Consts;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -130,4 +131,48 @@ public class SingerController {
         return singerService.searchSinger();
     }
 
+    @PostMapping("/updateImg")
+    public Object updateImgFile(@RequestParam("file") MultipartFile imgFile,@RequestParam("id") int id){
+        JSONObject jsonObject = new JSONObject();
+        if(imgFile.isEmpty()){
+            jsonObject.put(Consts.CODE,0);
+            jsonObject.put(Consts.MSG,"上传图片失败");
+            return jsonObject;
+        }
+        //文件名=当前时间到毫秒+原来文件名
+        String fileName = System.currentTimeMillis()+imgFile.getOriginalFilename();
+        //文件路径
+        String filePath = System.getProperty("user.dir")+System.getProperty("file.separator")+"img"
+                +System.getProperty("file.separator")+"singerPic";
+        File file = new File(filePath);
+        //若不存在文件路径自己创建一个
+        if(!file.exists()){
+            file.mkdir();
+        }
+        //实际文件地址
+        File dest = new File(filePath+System.getProperty("file.separator")+fileName);
+        //存储到数据库中的相对文件地址
+        String storeAvatorPath = "/img/singerPic/"+fileName;
+        try {
+            imgFile.transferTo(dest);//上传到项目中
+            Singer singer = new Singer();
+            singer.setId(id);
+            singer.setPic(storeAvatorPath);
+            boolean flag = singerService.updateSinger(singer);//更新相对路径到数据库中
+            if(flag){//上传成功
+                jsonObject.put(Consts.CODE,1);
+                jsonObject.put(Consts.MSG,"上传图片成功");
+                jsonObject.put("pic",storeAvatorPath);
+                return jsonObject;
+            }
+            jsonObject.put(Consts.CODE,0);
+            jsonObject.put(Consts.MSG,"上传图片失败");
+            return jsonObject;
+        } catch (IOException e) {
+            jsonObject.put(Consts.CODE,0);
+            jsonObject.put(Consts.MSG,"上传图片失败"+e.getMessage());
+        } finally {
+            return jsonObject;
+        }
+    }
 }
